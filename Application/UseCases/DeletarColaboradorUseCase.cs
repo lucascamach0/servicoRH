5d7118d72dcec15702058ad5b1fa9d810b5ee3f9
Fernanda Gavros <fernanda.gavros@animaeducacao.com.br>
2022-10-11 08:29:33 -0300

    criação da condição no UseCase DeletarColaborador

--- Application/UseCases/DeletarColaboradorUseCase.cs
+++ Application/UseCases/DeletarColaboradorUseCase.cs
@@ -1,16 +1,28 @@
-﻿using ServicoRH.Infra;
+﻿using ServicoRH.Domain;
+using ServicoRH.Infra;
 
 namespace ServicoRH.Application.UseCases
 {
     public class DeletarColaboradorUseCase
     {
         private readonly ColaboradorRepository _colaboradorRepository;
+        private readonly RetornarDadosDoColaboradorUseCase _retornarDadosDoColaboradorUseCase;
+
         public DeletarColaboradorUseCase()
         {
             _colaboradorRepository = new ColaboradorRepository();
+            _retornarDadosDoColaboradorUseCase = new RetornarDadosDoColaboradorUseCase();
         }
         public string ExcluirColaborador(string cpf)
         {
+            Colaborador colaboradorResposta = new Colaborador();
+
+            colaboradorResposta = _retornarDadosDoColaboradorUseCase.ObterDadosDoColaborador(cpf);
+
+            if (colaboradorResposta.Cpf == null)
+            {
+                return "Usuário não encontrado";
+            }
             return _colaboradorRepository.DeletarColaborador(cpf);
         }
 
