87de4a5cb6a35e3d364bbcba5cad4f8716ec36e1
unknown <trza.controller@trza.com.br>
2022-10-24 09:20:09 -0300

    add test unit

--- Application/UseCases/RetornarListaDeCargosUseCase.cs
+++ Application/UseCases/RetornarListaDeCargosUseCase.cs
@@ -14,7 +14,26 @@ namespace ServicoRH.Application.UseCases
         public List<Cargo> ObterListaDeCargos()
         {
             List<Cargo> cargos = _cargoRepository.ObterTodosOsCargos();
+
             return cargos;
+        }
+
+        private Cargo ObterCargoQA()
+        {
+            List<Cargo> cargos = _cargoRepository.ObterTodosOsCargos();
+
+            foreach (var cargoAuxilar in cargos)
+            {
+                if (cargoAuxilar.Nome.Equals("QA"))
+                {
+                    return cargoAuxilar;
+                }
+            }
+
+            var cargo = cargos.Where(a => a.Nome.Equals("QA")).FirstOrDefault();
+            var cargosABC = cargos.Where(a => a.Nome.Equals("Squad Leader") || a.Nome.Equals("QA"));
+
+            return cargo;
         }
     }
 }
