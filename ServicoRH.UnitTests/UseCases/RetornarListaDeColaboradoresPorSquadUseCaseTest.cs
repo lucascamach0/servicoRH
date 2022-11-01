621f0f1675ad5493bc1bf3e34a89984b85fc020f
Fernanda Gavros <fernanda.gavros@animaeducacao.com.br>
2022-11-01 08:32:48 -0300

    Teste unitário

---
+++ ServicoRH.UnitTests/UseCases/RetornarListaDeColaboradoresPorSquadUseCaseTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using ServicoRH.Application.UseCases;
+using ServicoRH.Domain;
+using ServicoRH.Infra.Interface;
+using Xunit;
+
+namespace ServicoRH.UnitTests.UseCases
+{
+    public class RetornarListaDeColaboradoresPorSquadUseCaseTest
+    {
+        private readonly Mock<IColaboradorRepository> _repositoryMock = new Mock<IColaboradorRepository>();
+
+        [Fact]
+        public void RetornarListaDeColaboradoresPorSquadComSucesso()
+        {
+            //Arrage
+            List<Colaborador> listaDeColaborador = new List<Colaborador>();
+            Colaborador colaborador = new Colaborador();
+            colaborador.Nome = "João";
+            listaDeColaborador.Add(colaborador);
+            // It.IsAny<string simula o parametro
+            _repositoryMock.Setup(a => a.BucarTodosOsColaboradorPorSquad(It.IsAny<string>())).Returns(listaDeColaborador);
+
+            RetornarListaDeColaboradoresPorSquadUseCase useCase = new RetornarListaDeColaboradoresPorSquadUseCase(_repositoryMock.Object);
+
+            //Action
+            var listaRetorno = useCase.ObterTodosOsDadosDoColaboradorPorSquad("squadTest");
+
+            //Assert
+            Assert.NotNull(listaRetorno);
+            Assert.Single(listaRetorno);
+            Assert.True(listaRetorno.Select(a => a.Nome.Equals("João")).FirstOrDefault());
+        }
+    }
+}
---
+++ ServicoRH.UnitTests/UseCases/RetornarSalarioPorCpfUseCaseTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using ServicoRH.Application.UseCases;
+using ServicoRH.Domain;
+using ServicoRH.Infra.Interface;
+using Xunit;
+
+namespace ServicoRH.UnitTests.UseCases
+{
+    public class RetornarSalarioPorCpfUseCaseTest
+    {
+        private readonly Mock<IColaboradorRepository> _repositoryMock = new Mock<IColaboradorRepository>();
+
+        [Fact]
+        public void RetornarSalarioPorCpfComSucesso()
+        {
+            Colaborador colaborador = new Colaborador();
+            colaborador.Salario = 1000;
+            //setup é o metodo de simulação do mock
+            // expressão lâmbida que vai simular o método do repository
+            // returns que vai fazer a simulação do retorno e passa o retorno que eu quero simular
+            // It.IsAny<string simula o parametro
+            //instancia do usecase real
+            //object é um atributo que fala que é um mock
+            _repositoryMock.Setup(a => a.BuscarColaboradorPorCpf(It.IsAny<string>())).Returns(colaborador);
+            RetornarSalarioPorCpfUseCase useCase = new RetornarSalarioPorCpfUseCase(_repositoryMock.Object);
+
+            var retorno = useCase.obterSalarioPorCpf("teste");
+
+            Assert.Equal(1000, retorno);
+
+        }
+    }
+}
